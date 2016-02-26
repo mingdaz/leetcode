aeb24ebaaad5c517715fa5a51924077fbf8849e7
mingdaz <zhangmd406@gmail.com>
2016-02-26 15:39:59 -0500

    Update leet033.cpp

--- leet033/leet033.cpp
+++ leet033/leet033.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-    	int l=0,h=nums.size();
-	int mid;
-	while(l<=h){
-		mid = l+(h-l)/2;
-		if(tagert>nums[mid]) l = mid+1;
-		else if(target<nums[mid]) h = mid-1;
-		else return mid;
-	}    
-	return -1;
+        int l = 0;
+        int r = nums.size()-1;
+        int mid;
+        while(l<=r){
+            mid =(l+r)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>nums[r]){
+                if(nums[mid]>target && target>=nums[l]) r=mid-1;
+                else l= mid+1;
+            }
+            else if(nums[l]>nums[mid]){
+                if(nums[mid]<target && target<=nums[r]) l = mid+1;
+                else r = mid-1;
+            }
+            else{
+                if(nums[mid]<target) l = mid+1;
+                else r = mid-1;
+            }
+        }
+        return -1;
     }
 };
