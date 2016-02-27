4cf601e3c6569bc248a29728d18757aa1da35843
mingdaz <zhangmd406@gmail.com>
2016-02-27 01:38:16 -0500

    Update leet042.cpp

--- leet042/leet042.cpp
+++ leet042/leet042.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int left = 0, right = height.size()-1;
-    if(right < 2) return 0;
-    int leftMax = height[left] , rightMax = height[right], second;
-    int area = 0;
-    while(left < right){
-        leftMax = max(leftMax, height[left]);
-        rightMax = max(rightMax, height[right]);
-        second = min(leftMax, rightMax);
-        if(height[left] < height[right]){
-            area += second - height[left];
-            left++;
-        }else{
-            area += second - height[right];
-            right--;
+        if(right < 2) return 0;
+        int leftMax = height[left] , rightMax = height[right], second;
+        int area = 0;
+        // key point is that, from left and right to the highest point. 
+        // The height(plus contained water) is increasing.
+        while(left < right){
+            leftMax = max(leftMax, height[left]);
+            rightMax = max(rightMax, height[right]);
+            second = min(leftMax, rightMax);
+            if(height[left] < height[right]){
+                area += second - height[left];
+                left++;
+            }else{
+                area += second - height[right];
+                right--;
+            }
         }
-    }
-    return area;
-            
-       
+        return area;
     }
 };
