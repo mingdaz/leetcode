0fbcd374e765ca82673ac3df9c31cbe9c8ae4a6f
mingdaz <zhangmd406@gmail.com>
2017-12-29 09:57:18 -0800

    Update leet533_1.cpp

--- leet533/leet533_1.cpp
+++ leet533/leet533_1.cpp
@@ -1,23 +1,20 @@
-
 class Solution {
 public:
     int findBlackPixel(vector<vector<char>>& picture, int N) {
-        unordered_map<string, int> um;
         int m = picture.size(), n = m == 0 ? 0 : picture[0].size();
         if(m == 0 || n == 0) return 0;
+        unordered_map<string, int> um;
         vector<int> colcnt(n, 0);
         for(int i = 0;i < m;i++) {
-            string s = "";
             int rowcnt = 0;
             for(int j = 0;j < n;j++) {
-                s += picture[i][j];
                 if(picture[i][j] == 'B') { 
                     colcnt[j]++;
                     rowcnt++;
                 }
             }
             if(rowcnt == N)
-                um[s]++;
+                um[string(picture[i].data(), n)]++;
         }
         int res = 0;
         for(auto &e: um) {
